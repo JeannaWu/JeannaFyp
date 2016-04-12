35859d1204208fd3656d39ea664065705123b3ef
ZHIYUN WU <wzyunzx@163.com>
2016-04-12 17:13:15 +0100

    Update the database

--- db/schema.rb
+++ db/schema.rb
@@ -87,13 +87,6 @@ ActiveRecord::Schema.define(version: 20160412090658) do
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
-  create_table "types", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "string",     limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.boolean  "admin",                  limit: 1
     t.string   "email",                  limit: 255,   default: "",    null: false
