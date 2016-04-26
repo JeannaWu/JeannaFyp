ab1fe60985b6a7464eb56e1565f47e8ae14a31db
ZHIYUN WU <wzyunzx@163.com>
2016-04-26 19:37:55 +0100

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415120912) do
+ActiveRecord::Schema.define(version: 20160426182915) do
 
   create_table "approval_downvotes", force: :cascade do |t|
     t.integer  "post_id",    limit: 4
@@ -107,13 +107,6 @@ ActiveRecord::Schema.define(version: 20160415120912) do
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
-  create_table "types", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "string",     limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.boolean  "admin",                  limit: 1
     t.string   "email",                  limit: 255,   default: "",    null: false
@@ -135,6 +128,8 @@ ActiveRecord::Schema.define(version: 20160415120912) do
     t.boolean  "paneluser",              limit: 1,     default: false
     t.string   "oauth_token",            limit: 255
     t.datetime "oauth_expires_at"
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
