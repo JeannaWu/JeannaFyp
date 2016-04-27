822fe707db34a33b86e5165a8915371c127a72d6
ZHIYUN WU <wzyunzx@163.com>
2016-04-27 08:55:41 +0100

    Change

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,6 +2,8 @@ RailsAdmin.config do |config|
   config.authorize_with do
     redirect_to main_app.root_path unless current_user.admin == true
   end
+
+  config.assets.initialize_on_precompile = false
   ### Popular gems integration
 
   ## == Devise ==
