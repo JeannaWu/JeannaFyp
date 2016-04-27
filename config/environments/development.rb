7dba7f946734982b76058292d27a87646df5273c
ZHIYUN WU <wzyunzx@163.com>
2016-04-27 09:13:46 +0100

    change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
   config.assets.initialize_on_precompile = false
-  
+   config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
   ENV['FACEBOOK_APP_ID'] = "983447761748095";
   ENV['FACEBOOK_SECRET'] = "51537d3016d16513a7c699eb5db19326";
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.precompile += ['admin.js', 'admin.css', 'swfObje
 # Rails.application.config.assets.paths << Emoji.images_path
 
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
-
+Rails.application.config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
