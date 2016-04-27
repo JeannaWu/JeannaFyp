e2fcc2f9420f472f1d66cfd490e96b1ca72bfcba
ZHIYUN WU <wzyunzx@163.com>
2016-04-27 09:04:22 +0100

    Change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+ 
+ config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
